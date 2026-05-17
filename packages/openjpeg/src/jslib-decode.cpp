0dc3d1639d66c5a43f2045af367949cf3eb79a0e
Ahmed.Ezzat <ahmedezzateasa@gmail.com>
2026-05-17 16:10:08 +0300

    debug: add verbose decode logs + ASSERTIONS=2 + SAFE_HEAP to trace error origin

--- packages/openjpeg/src/jslib-decode.cpp
+++ packages/openjpeg/src/jslib-decode.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) Chris Hafey.
 // SPDX-License-Identifier: MIT
-// BUILD_VERSION: 2026-05-17-fix-table-growth
-
+// BUILD_VERSION: 2026-05-17-debug-logging
 
 #include "J2KDecoder.hpp"
 #include "FrameInfo.hpp"
@@ -10,8 +9,76 @@
 
 #include <emscripten.h>
 #include <emscripten/bind.h>
+#include <stdio.h>
 
 using namespace emscripten;
+
+// ---------------------------------------------------------------------------
+// Debug-instrumented thin wrappers around J2KDecoder
+// These emit console-visible printf output so we can trace the exact point
+// of failure when the WASM trap occurs.
+// ---------------------------------------------------------------------------
+
+struct DebugJ2KDecoder {
+  J2KDecoder inner;
+
+  emscripten::val getEncodedBuffer(size_t encodedSize) {
+    printf("[DBG] getEncodedBuffer(%zu)\n", encodedSize);
+    auto result = inner.getEncodedBuffer(encodedSize);
+    printf("[DBG] getEncodedBuffer OK\n");
+    return result;
+  }
+
+  emscripten::val getDecodedBuffer() {
+    printf("[DBG] getDecodedBuffer()\n");
+    auto result = inner.getDecodedBuffer();
+    printf("[DBG] getDecodedBuffer OK\n");
+    return result;
+  }
+
+  void readHeader() {
+    printf("[DBG] readHeader()\n");
+    inner.readHeader();
+    printf("[DBG] readHeader OK\n");
+  }
+
+  Size calculateSizeAtDecompositionLevel(int decompositionLevel) {
+    printf("[DBG] calculateSizeAtDecompositionLevel(%d)\n", decompositionLevel);
+    auto result = inner.calculateSizeAtDecompositionLevel(decompositionLevel);
+    printf("[DBG] calculateSizeAtDecompositionLevel OK -> %dx%d\n", result.width, result.height);
+    return result;
+  }
+
+  void decode() {
+    printf("[DBG] decode() - about to call openjpeg\n");
+    inner.decode();
+    printf("[DBG] decode() OK\n");
+  }
+
+  void decodeSubResolution(int decompositionLevel, int quality) {
+    printf("[DBG] decodeSubResolution(level=%d, quality=%d)\n", decompositionLevel, quality);
+    inner.decodeSubResolution(decompositionLevel, quality);
+    printf("[DBG] decodeSubResolution OK\n");
+  }
+
+  FrameInfo getFrameInfo() {
+    printf("[DBG] getFrameInfo()\n");
+    auto fi = inner.getFrameInfo();
+    printf("[DBG] getFrameInfo OK -> %dx%d bpp=%d comps=%d signed=%d\n",
+           fi.width, fi.height, fi.bitsPerSample, fi.componentCount, (int)fi.isSigned);
+    return fi;
+  }
+
+  int getNumDecompositions()  { return inner.getNumDecompositions(); }
+  bool getIsReversible()      { return inner.getIsReversible(); }
+  int getProgressionOrder()   { return inner.getProgressionOrder(); }
+  Point getImageOffset()      { return inner.getImageOffset(); }
+  Size getTileSize()          { return inner.getTileSize(); }
+  Point getTileOffset()       { return inner.getTileOffset(); }
+  Size getBlockDimensions()   { return inner.getBlockDimensions(); }
+  int getNumLayers()          { return inner.getNumLayers(); }
+  int getColorSpace()         { return inner.getColorSpace(); }
+};
 
 EMSCRIPTEN_BINDINGS(FrameInfo) {
   value_object<FrameInfo>("FrameInfo")
@@ -38,23 +105,23 @@ EMSCRIPTEN_BINDINGS(Size) {
 }
 
 EMSCRIPTEN_BINDINGS(J2KDecoder) {
-  class_<J2KDecoder>("J2KDecoder")
+  class_<DebugJ2KDecoder>("J2KDecoder")
     .constructor<>()
-    .function("getEncodedBuffer", &J2KDecoder::getEncodedBuffer)
-    .function("getDecodedBuffer", &J2KDecoder::getDecodedBuffer)
-    .function("readHeader", &J2KDecoder::readHeader)
-    .function("calculateSizeAtDecompositionLevel", &J2KDecoder::calculateSizeAtDecompositionLevel)
-    .function("decode", &J2KDecoder::decode)
-    .function("decodeSubResolution", &J2KDecoder::decodeSubResolution)
-    .function("getFrameInfo", &J2KDecoder::getFrameInfo)
-    .function("getNumDecompositions", &J2KDecoder::getNumDecompositions)
-    .function("getIsReversible", &J2KDecoder::getIsReversible)
-    .function("getProgressionOrder", &J2KDecoder::getProgressionOrder)
-    .function("getImageOffset", &J2KDecoder::getImageOffset)
-    .function("getTileSize", &J2KDecoder::getTileSize)
-    .function("getTileOffset", &J2KDecoder::getTileOffset)
-    .function("getBlockDimensions", &J2KDecoder::getBlockDimensions)
-    .function("getNumLayers", &J2KDecoder::getNumLayers)
-    .function("getColorSpace", &J2KDecoder::getColorSpace)
+    .function("getEncodedBuffer",                 &DebugJ2KDecoder::getEncodedBuffer)
+    .function("getDecodedBuffer",                 &DebugJ2KDecoder::getDecodedBuffer)
+    .function("readHeader",                       &DebugJ2KDecoder::readHeader)
+    .function("calculateSizeAtDecompositionLevel",&DebugJ2KDecoder::calculateSizeAtDecompositionLevel)
+    .function("decode",                           &DebugJ2KDecoder::decode)
+    .function("decodeSubResolution",              &DebugJ2KDecoder::decodeSubResolution)
+    .function("getFrameInfo",                     &DebugJ2KDecoder::getFrameInfo)
+    .function("getNumDecompositions",             &DebugJ2KDecoder::getNumDecompositions)
+    .function("getIsReversible",                  &DebugJ2KDecoder::getIsReversible)
+    .function("getProgressionOrder",              &DebugJ2KDecoder::getProgressionOrder)
+    .function("getImageOffset",                   &DebugJ2KDecoder::getImageOffset)
+    .function("getTileSize",                      &DebugJ2KDecoder::getTileSize)
+    .function("getTileOffset",                    &DebugJ2KDecoder::getTileOffset)
+    .function("getBlockDimensions",               &DebugJ2KDecoder::getBlockDimensions)
+    .function("getNumLayers",                     &DebugJ2KDecoder::getNumLayers)
+    .function("getColorSpace",                    &DebugJ2KDecoder::getColorSpace)
    ;
 }
